c677fda07a5cf734920fec88229d1feb786c224a
Cayke Manjanelli Xavier <cayke@hotmail.com>
2022-07-09 10:09:57 -0300

    feat: init controller class

--- src/main/java/com/manjanelli/healthcheck/app.java
+++ src/main/java/com/manjanelli/healthcheck/app.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthcheckApplication {
+public class app {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthcheckApplication.class, args);
+		SpringApplication.run(app.class, args);
 	}
 
 }
